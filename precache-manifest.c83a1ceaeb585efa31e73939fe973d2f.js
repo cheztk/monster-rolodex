4a309755378b1095821d12a92337e961fe6a0bf9
Chezt <ckengchon@gmail.com>
2019-07-08 18:01:44 +0700

    Updates

---
+++ precache-manifest.c83a1ceaeb585efa31e73939fe973d2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8020aca739cd5e3ee1ec7052bfba7570",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3a3511ed0406f3ef51ff",
+    "url": "/monster-rolodex/static/css/main.96fc47eb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3a3511ed0406f3ef51ff",
+    "url": "/monster-rolodex/static/js/main.eeed742e.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
